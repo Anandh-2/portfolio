a65f56239f0cbc91f0d1e3aab8899bab9648cc27
Anandh-2 <anandh02102004@gmail.com>
2025-01-16 20:09:40 +0530

    Changes in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ProjectBox from "./ProjectBox";
 import logo from '../images/GitHub.jpg';
+import ToDo from '../images/To-Do App.png';
+import RUser from '../images/Random User.png';
 
 export default function Projects() {
   const projects = [
+    {title: "To-Do App", description: "My first full stack application.", image:ToDo ,link: "https://to-does.netlify.app/"},
+    {title: "Random User Generator", description: "A random user generator application using Random User API.", image:RUser ,link: "https://get-randomuser.netlify.app/"},
     {title: "Real Time Chat", description: "Real time chat application developed at CoderOne internship.", image: logo, link:'https://github.com/Asif-khan-this-is/Real-time-chat-application'},
     {title: "Feedback Collection", description: "This is the feedback collection system developed at CoderOne internship.", image: logo, link:'https://github.com/neerajbhasin-8055/Feedback-Collection-System'},
-    {title: "Tic Tac Toe", description: "Tic Tac Toe game for fun.", image: logo, link:'https://github.com/Anandh-2/tic-tac-toe'},
-    {title: "Colorify", description: "My first javascript project.", image: logo, link:'https://github.com/Anandh-2/Colorify'},
-    {title: "Live Chat", description: "Live chat application.", image: logo, link:'https://github.com/Anandh-2/live-chat'},
   ];
   return (
     <section id="projects">
